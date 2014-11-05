772e8f1ee73cc9b7c5afaa623a96d2e2cfc54810
Adriel <Adriel@10.1.248.248>
2014-11-05 19:14:56 -0200

    Fazendo commit inicial do projeto

---
+++ src/projetogitjava/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package projetogitjava;
+
+/**
+ *
+ * @author Adriel
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Utilizando o Git através do NetBeans!");
+    }
+    
+}
